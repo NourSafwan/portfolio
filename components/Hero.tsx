f621cf16c31f40de3fb4652b575a9035862decb9
NourSafwan <nouraldin374@gmail.com>
2025-01-15 09:23:21 +0200

    refactor: Update Hero component text for clarity and creativity

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,14 +26,14 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-center text-xs text-blue-100 max-w-80">
-            dynamic web magic with next.js
+            Bringing ideas to life with code and creativity.
           </h2>
           <TextGenerateEffect
             words="transforming concepts into seamless experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl capitalize"
           />
           <p className="text-center capitalize md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            hi, i&apos;m <span className="text-purple">Nour</span>, a Front-End
+            hi, i&apos;m <span className="text-purple">Nour</span>, a Full-Stack
             developer based in egypt.
           </p>
           <a href="#about">
